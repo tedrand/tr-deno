52728e8a44d6b23857c7dfb7921d0f1cf50d3138
Theodore Rand <randtheodore@gmail.com>
2020-05-21 19:42:02 -0400

    ripped out svelte crap, finished new home page

--- main.ts
+++ main.ts
@@ -1,23 +1,21 @@
 import { Application } from "https://deno.land/x/abc/mod.ts";
 import { renderFile } from "https://deno.land/x/dejs/mod.ts";
-
 import { getPort } from "./server/utils.ts";
 
-const app = new Application()
-
-app.static("/public", "public")
+const app = new Application();
 
 app.renderer = {
     render<T>(name: string, data: T): Promise<Deno.Reader> {
         return renderFile(name, data);
-    }
+    },
 };
 
 app
-    .get("/", async(c) => {
-        await c.render("./public/index.html", {
-            name: "Home | TedRand.com"
-        })
+    .static("/static", "assets")
+    .get("/", async (c) => { 
+        await c.render("./public/index.ejs", { 
+            name: "Ted Rand" 
+        });
     })
     .start({ port: getPort() });
 
