cb1f28cd703cc2b0427ce645248f57bda72cff55
Theodore Rand <randtheodore@gmail.com>
2020-05-21 14:21:18 -0400

    switched to render live file so that certain head contents can be inserted server-side

--- main.ts
+++ main.ts
@@ -1,12 +1,26 @@
 import { Application } from "https://deno.land/x/abc/mod.ts";
+import { renderFile } from "https://deno.land/x/dejs/mod.ts";
+
 import { getPort } from "./server/utils.ts";
 
 const app = new Application()
 
 app.static("/public", "public")
 
-app.file("/", "public/index.html")
+app.renderer = {
+    render<T>(name: string, data: T): Promise<Deno.Reader> {
+        return renderFile(name, data);
+    }
+};
+
+app
+    .get("/", async(c) => {
+        await c.render("./public/index.html", {
+            name: "Home | TedRand.com"
+        })
+    })
+    .start({ port: getPort() });
 
 console.log(`🦕 Listening on ${getPort()}`);
-await app.start({ port: getPort() });
+
 
