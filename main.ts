08b3067fdcfbe4fb919c17b500f2970a3f566651
Theodore Rand <randtheodore@gmail.com>
2020-05-22 02:12:13 -0400

    connected static page routes to flask api (cruftily)

--- main.ts
+++ main.ts
@@ -1,9 +1,6 @@
 import { Application } from "https://deno.land/x/abc/mod.ts";
 import { renderFile } from "https://deno.land/x/dejs/mod.ts";
-import { 
-    getPort, 
-    setStaticHeaders 
-} from "./server/utils.ts";
+import { getPort, setStaticHeaders } from "./server/utils.ts";
 
 const app = new Application();
 
@@ -24,6 +21,45 @@ app.get("/", async (c) => {
     await c.render("./public/index.ejs", { 
         name: "Ted Rand" 
     });
+})
+
+app.get("/about", async (c) => {
+    let ctx = c
+    await fetch(`https://pycourt.herokuapp.com/contents/about`)
+        .then((resp) => resp.json())
+        .then(async function(data) {
+            console.log(data)
+            await ctx.render("./public/staticPage.ejs", {
+                data: data[0]
+            });
+            
+        })
+})
+
+app.get("/terms", async (c) => {
+    let ctx = c
+    await fetch(`https://pycourt.herokuapp.com/contents/terms`)
+        .then((resp) => resp.json())
+        .then(async function(data) {
+            console.log(data)
+            await ctx.render("./public/staticPage.ejs", {
+                data: data[0]
+            });
+            
+        })
+})
+
+app.get("/privacy", async (c) => {
+    let ctx = c
+    await fetch(`https://pycourt.herokuapp.com/contents/privacy`)
+        .then((resp) => resp.json())
+        .then(async function(data) {
+            console.log(data)
+            await ctx.render("./public/staticPage.ejs", {
+                data: data[0]
+            });
+            
+        })
 })
 
 app.start({ port: getPort() });
--- server/utils.ts
+++ server/utils.ts
@@ -1,6 +1,7 @@
 import { MiddlewareFunc } from "https://deno.land/x/abc/mod.ts";
 import * as flags from "https://deno.land/std/flags/mod.ts";
 
+// Determine if the app is running from a specific port in production
 export function getPort() {
     const { args, exit } = Deno;
     const argPort = flags.parse(args).port;
@@ -12,6 +13,7 @@ export function getPort() {
     return port;
 }
 
+// Set headers for different types of static files that get sent across the server
 export const setStaticHeaders: MiddlewareFunc = next => c => {
     if (c.path.indexOf("/static/images/") === 0) {
         c.response.headers = new Headers({
