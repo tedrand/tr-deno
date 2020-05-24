2346875954dea8a3f5a95228c52f715cdb1641f0
Theodore Rand <randtheodore@gmail.com>
2020-05-24 12:04:06 -0400

    resized two more images and added cache validators to middleware

--- server/middleware/headersMiddleware.ts
+++ server/middleware/headersMiddleware.ts
@@ -5,7 +5,8 @@ export const setStaticHeaders: MiddlewareFunc = next => c => {
     if (c.path.indexOf("/static/images/") === 0) {
         c.response.headers = new Headers({
             "cache-control": "public, max-age=2592000",
-            "expires": new Date(Date.now() + 2592000000).toUTCString()
+            "expires": new Date(Date.now() + 2592000000).toUTCString(),
+            "last-modified": new Date(Date.now()).toUTCString()
         })
     }
     
@@ -15,7 +16,8 @@ export const setStaticHeaders: MiddlewareFunc = next => c => {
 export const setPageHeaders: MiddlewareFunc = next => c => {
     c.response.headers = new Headers({
         "cache-control": "public, max-age=604800, immutable",
-        "expires": new Date(Date.now() + 604800).toUTCString()
+        "expires": new Date(Date.now() + 604800).toUTCString(),
+        "last-modified": new Date(Date.now()).toUTCString()
     });
 
     return next(c)
